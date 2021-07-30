57bda97dba627006c833afd6811c626dd9cdd817
Karsten Navarro <86648586+karsten-navarro@users.noreply.github.com>
2021-07-30 13:29:27 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CURRENT_TAG="v1.3.0"
+INT=$(grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$CURRENT_TAG") 
+MAJOR=$(cut -d'.' -f1 <<< "$INT")
+MINOR=$(cut -d'.' -f2 <<< "$INT")
+PATCH=$(cut -d'.' -f3 <<< "$INT")
+
+MAJOR=$(($MAJOR+1))
+# Value to increment (vX.Y.Z) == (vMAJOR.MINOR.PATCH)
+
+NEW_TAG="v$MAJOR.$MINOR.$PATCH"
+
+echo $NEW_TAG
+
+exit 0
